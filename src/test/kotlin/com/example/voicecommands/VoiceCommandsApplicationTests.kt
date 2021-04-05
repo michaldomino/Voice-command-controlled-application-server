72c437db4a0eea6a3d1b7940ef42ac00c3408442
Michał Domino <michal.domino@yahoo.com>
2021-04-05 11:38:29 +0200

    first commit

---
+++ src/test/kotlin/com/example/voicecommands/VoiceCommandsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.voicecommands
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VoiceCommandsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
